5ec5d659bbfccff56c02ec447b345445dfb8caa4
tcdw <tcdw2011@gmail.com>
2018-05-01 15:17:32 +0800

    自动判断是否为元素

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ class Pomment {
         title = document.title,
         url = document.location.href,
     } = {}) {
+        if (!(element instanceof Element)) {
+            this.element = document.querySelector(element);
+        } else {
+            this.element = element;
+        }
         if (typeof server !== 'string') {
             throw new TypeError('Value `server` is required');
         }
