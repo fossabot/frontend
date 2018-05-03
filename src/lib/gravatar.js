293e7f4ed13ed25ec6954c85b0740b6489175ff3
tcdw <tcdw2011@gmail.com>
2018-05-03 18:43:06 +0800

    完善文档

--- src/lib/gravatar.js
+++ src/lib/gravatar.js
@@ -1,3 +1,9 @@
+/**
+ * 获取 Gravatar 头像地址
+ * @param {string} prefix           使用的服务器，以斜杠结尾
+ * @param {string} hash             头像对应电子邮箱地址的 MD5 值
+ * @param {number} [baseSize = 56]  头像大小。默认 56
+ */
 const avatarURL = (prefix, hash, baseSize = 56) => `${prefix}${hash}?s=${window.devicePixelRatio * baseSize}`;
 
 export default avatarURL;
