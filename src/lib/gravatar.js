8deb85d87c3758ea499298f14944693ef718fd93
tcdw <tcdw2011@gmail.com>
2018-05-03 18:43:35 +0800

    完善文档继续

--- src/lib/gravatar.js
+++ src/lib/gravatar.js
@@ -3,6 +3,7 @@
  * @param {string} prefix           使用的服务器，以斜杠结尾
  * @param {string} hash             头像对应电子邮箱地址的 MD5 值
  * @param {number} [baseSize = 56]  头像大小。默认 56
+ * @returns {string}                最终得到的地址
  */
 const avatarURL = (prefix, hash, baseSize = 56) => `${prefix}${hash}?s=${window.devicePixelRatio * baseSize}`;
 
