4c8dc4f7f0e411f068826642eb8515e8e059743e
tcdw <tcdw2011@gmail.com>
2018-05-06 02:24:03 +0800

    开始简体中文翻译

--- src/i18n/data/index.js
+++ src/i18n/data/index.js
@@ -1,8 +1,11 @@
 /* eslint-disable camelcase */
 
 import en_US from './en-US';
+import zh_CN from './zh-CN';
 
 export default {
+    'zh-CN': zh_CN,
+    cn: zh_CN,
     'en-US': en_US,
     en: en_US,
 };
---
+++ src/i18n/data/zh-CN.js
@@ -0,0 +1,7 @@
+export default {
+    __name__: 'Chinese (Simplified)',
+    btnCancel: '取消',
+    timeSinceOther(data) {
+        return `${data.year} 年 ${data.day} 月 ${data.month} 日`;
+    },
+};
